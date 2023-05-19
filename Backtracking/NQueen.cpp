bf54ba27277ef540f03b9a15e13a950d2adcca1e
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-05-19 10:41:23 +0530

    Update NQueen.cpp

--- Backtracking/NQueen.cpp
+++ Backtracking/NQueen.cpp
@@ -1,19 +1,28 @@
+//Time Complexity:O(N!)
+//Space Complexity: O(N^2)
+
 #include<bits/stdc++.h>
 using namespace std;
 
 bool isSafe(int res[10][10], int row, int col, int n)
 {
+    //Same column
+   
     for(int i=0; i<row; i++)
     {
         if(res[i][col] == 1)
         return false;
     }
+    
+    //Upper Left Diagonal 
 
     for(int i=row, j=col; i>=0 && j>=0; i--, j--)
     {
         if(res[i][j] == 1)
         return false;
     }
+    
+    //Upper Right Diagonal
 
     for(int i=row, j=col; i>=0 && j<n; i--, j++)
     {
@@ -26,6 +35,9 @@ bool isSafe(int res[10][10], int row, int col, int n)
 
 void NQueen(int res[10][10], int row, int n)
 {
+    //We have reached one possible solution by placing all queens correctly
+    //Print and return
+    
     if(row == n)
     {
         for(int r = 0; r < n; r++)
@@ -38,6 +50,8 @@ void NQueen(int res[10][10], int row, int n)
         cout << endl;
         return;
     }
+    
+    //Check all possible solutions
 
     for(int col=0; col<n; col++)
     {
@@ -45,6 +59,9 @@ void NQueen(int res[10][10], int row, int n)
         {
             res[row][col] = 1;
             NQueen(res, row+1, n);
+            
+            //Backtrack if this is not a correct position
+            
             res[row][col] = 0;
         }
     }
