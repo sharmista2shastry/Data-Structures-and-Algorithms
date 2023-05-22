53c6c5ea83f9a4313417b008c4eff7d07d1fc16b
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-05-22 10:41:29 +0530

    Update SubsetSum.cpp

--- Backtracking/SubsetSum.cpp
+++ Backtracking/SubsetSum.cpp
@@ -1,44 +1,55 @@
 //Time Complexity:It has a time complexity of O(2^n), where n is the size of the input array array
 //Space Complexity: The space complexity of the code is O(n), where n is the size of the input array arr. This is because the code uses recursion, and the space required for the recursive call stack grows with the depth of the recursion, which is equal to the size of the input array. 
 
-#include <bits/stdc++.h> 
-using namespace std; 
-int subsetCount(int arr[], int n, int index, int target) 
-{ 
-    if (index == n) 
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsetSum(int *arr, int n, int &count, int sum, int i)
+{
+    if(i == n)
     {
-        if (target == 0) 
-        return 1; 
-        
-        else return 0; 
-    } 
+        if(sum == 0)
+        {
+            count++;
+            return;
+        }
 
-    int count = 0; 
-
-    if (target - arr[index] >= 0) 
-    { 
-        count += subsetCount(arr, n, index + 1, target - arr[index]); 
-    } 
-
-    count += subsetCount(arr, n, index + 1, target); 
-    
-    return count;
-} 
-
-int main() 
-{ 
-    int t; cin >> t; 
-    while (t--) 
-    { 
-        int n, k; 
-        cin >> n >> k; 
-        int arr[n]; 
-
-        for (int i = 0; i < n; i++)
-        { 
-            cin >> arr[i]; 
-        } 
-
-        cout << subsetCount(arr, n, 0, k) << endl;
+        return;
     }
+
+    // Include current element in the sum and recurse
+    if (sum - arr[i] >= 0)
+    {
+        subsetSum(arr, n, count, sum - arr[i], i + 1);
+    }
+
+    // Exclude current element from the sum and recurse
+    subsetSum(arr, n, count, sum, i + 1);
+}
+
+int main(){
+    
+    // write your code here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int *arr = new int[n];
+        int sum;
+        cin >> sum;
+
+        for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+        int count = 0;
+        
+        subsetSum(arr, n, count, sum, 0);
+        cout << count << endl;
+
+        delete [] arr;
+    }
+
+    return 0;
 }
