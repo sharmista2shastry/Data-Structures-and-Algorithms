3f19073ebdc414f2eb01886da2286255057b2c04
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-06-04 16:05:18 +0530

    Update EditDistance.cpp

--- Dynamic Programming/EditDistance.cpp
+++ Dynamic Programming/EditDistance.cpp
@@ -28,3 +28,54 @@ int editDistance(string s1, string s2) {
 	return helper(s1, s2, s1.length(), s2.length());
 }
 
+//Using Memoization
+//Time Complexity: O(mn)
+
+int helper(string s1, string s2, int m, int n, int **ans)
+{
+	if(m == 0 || n == 0)
+	{
+		return max(m, n);
+	}
+
+	if(ans[m][n] != -1)
+	return ans[m][n];
+
+	if(s1[0] == s2[0])
+	return helper(s1.substr(1), s2.substr(1), m-1, n-1, ans);
+
+	int i = 1+ helper(s1.substr(1), s2, m-1, n, ans);
+	int d = 1+ helper(s1, s2.substr(1), m, n-1, ans);
+	int r = 1+ helper(s1.substr(1), s2.substr(1), m-1, n-1, ans);
+
+	ans[m][n] = min(i, min(d, r));
+	return ans[m][n];
+}
+
+int **allocate2DArray(int m, int n)
+{
+	int **ans = new int*[m+1];
+
+	for(int i=0; i<=m; i++)
+	{
+		ans[i] = new int[n+1];
+	}
+
+	for(int i=0; i<=m; i++)
+	{
+		for(int j=0; j<=n; j++)
+		{
+			ans[i][j] = -1;
+		}
+	}
+
+	return ans;
+}
+
+int editDistance(string s1, string s2) {
+	// Write your code here
+	int m = s1.length(), n = s2.length();
+	int **ans = allocate2DArray(m, n);
+
+	return helper(s1, s2, m, n, ans);
+}
