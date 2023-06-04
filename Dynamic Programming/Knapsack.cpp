e0ae3836539172ea8957f1a148a2b6c8b45a6255
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-06-04 20:09:28 +0530

    Update Knapsack.cpp

--- Dynamic Programming/Knapsack.cpp
+++ Dynamic Programming/Knapsack.cpp
@@ -70,4 +70,47 @@ int knapsack(int *weights, int *values, int n, int maxWeight)
 }
 
 //Using DP
+int helper(int *weights, int *values, int n, int W, int **ans)
+{
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=1; j<=W; j++)
+		{
+			if(weights[n-i] > j)
+			ans[i][j] = ans[i-1][j];
 
+			else
+			{
+				int x = ans[i-1][j-weights[n-i]] + values[n-i];
+				int y = ans[i-1][j];
+
+				ans[i][j]  = max(x,y);
+			}
+		}
+	}
+
+	return ans[n][W];
+}
+
+int **allocate2DArray(int n, int w)
+{
+	int **ans = new int*[n+1];
+
+	for(int i=0; i<=n; i++)
+	ans[i] = new int[w+1];
+
+	for(int i=0; i<=n; i++)
+	ans[i][0] = 0;
+
+	for(int j=0; j<=w; j++)
+	ans[0][j] = 0;
+
+	return ans;
+}
+
+int knapsack(int *weights, int *values, int n, int maxWeight)
+{
+	//write your code here
+	int **ans = allocate2DArray(n, maxWeight); 
+	return helper(weights, values, n, maxWeight, ans);
+}
