bf3fbb53e59f20d00d8abc1449badab1714ee8e0
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-06-05 15:53:24 +0530

    Update LongestIncreasingSubsequence.cpp

--- Dynamic Programming/LongestIncreasingSubsequence.cpp
+++ Dynamic Programming/LongestIncreasingSubsequence.cpp
@@ -1,29 +1,35 @@
-//Using Memoization
-
-int helper(int* arr, int n, int last, int *ans)
-{
-	if(n == 0)
-	return 0;
-
-	if(ans[n] != -1)
-	return ans[n];
-
-	if(arr[0] <= last)
-	return helper(arr+1, n-1, last, ans);
-
-	int x = 1 + helper(arr+1, n-1, arr[0], ans);
-	int y = helper(arr+1, n-1, last, ans);
-
-	ans[n] = max(x,y);
-	return ans[n];
-}
+//Time Complexity: O(n^2)
 
 int longestIncreasingSubsequence(int* arr, int n) {
 	// Write your code here
-	int *ans = new int[n+1];
+	int *output = new int[n];
 
-	for(int i=0; i<=n; i++)
-	ans[i] = -1;
+	output[0] = 1;
 
-	return helper(arr, n, -1, ans);
+	for(int i=1; i<n; i++)
+	{
+		output[i] = 1;
+
+		for(int j=i-1; j>=0; j--)
+		{
+			if(arr[j] >= arr[i])
+			continue;
+		
+			int count = 1 + output[j];
+
+			if(count > output[i])
+			output[i] = count;
+		}
+	}
+
+	int best = 0;
+
+	for(int i=0; i<n; i++)
+	{
+		if(best < output[i])
+		best = output[i];
+	}
+
+	delete [] output;
+	return best;
 }
