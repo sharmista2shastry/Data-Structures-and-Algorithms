aeed02979f2d5463e243af8dd9c67daff7fafac7
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-06-06 23:49:15 +0530

    Update WaysToMakeCoinChange.cpp

--- Dynamic Programming/WaysToMakeCoinChange.cpp
+++ Dynamic Programming/WaysToMakeCoinChange.cpp
@@ -78,3 +78,52 @@ int countWaysToMakeChange(int denominations[], int numDenominations, int value)
 
 	return helper(denominations, numDenominations, value, ans);
 }
+
+
+//Using DP
+
+#include <climits>
+#include <unordered_map>
+
+int helper(int denominations[], int numDenominations, int value, int **ans)
+{
+	for(int i=1; i<=numDenominations; i++)
+	{
+		for(int j=1; j<=value; j++)
+		{
+			int include = 0, exclude = 0;
+			
+			if(j - denominations[i - 1] >= 0)
+                    include = ans[i][j - denominations[i - 1]];
+
+                exclude = ans[i - 1][j];
+
+                ans[i][j] = include + exclude;
+		}
+	}
+
+	return ans[numDenominations][value];
+}
+
+int **allocate2DArray(int numDenominations, int value)
+{
+	int **ans = new int*[numDenominations + 1];
+
+	for(int i=0; i<=numDenominations; i++)
+	ans[i] = new int[value + 1];
+
+	return ans;
+}
+
+int countWaysToMakeChange(int denominations[], int numDenominations, int value) 
+{
+	int **ans = allocate2DArray(numDenominations, value);
+
+	for(int i=0; i<=numDenominations; i++)
+	ans[i][0] = 1;
+
+	for(int j=0; j<=value; j++)
+	ans[0][j] = 0;
+
+	return helper(denominations, numDenominations, value, ans);
+}
