b078979fd573137a5c4363bc587e8fbcb8af76f3
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-07-28 10:57:54 +0530

    Update twoSum.cpp

--- STL/twoSum.cpp
+++ STL/twoSum.cpp
@@ -38,3 +38,32 @@ vector<pair<int,int>> twoSum(vector<int>& arr, int target, int n)
 	
 	return ans;
 }
+
+//Alternate and slightly more efficient solution with O(nlogn) time complexity:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> twoSum(vector<int>& arr, int target, int n)
+{
+	// Write your code here. 
+	unordered_map<int, int> mp;
+	vector<pair<int,int>> ans;
+
+	for(int i=0; i<n; i++)
+	{
+		if(mp.count(target - arr[i]) > 0 && mp[target-arr[i]] != -1)
+		{
+			ans.push_back({arr[i], target - arr[i]});
+			mp[arr[i]] = -1;
+		}
+
+		else
+		mp[arr[i]] = i;
+	}
+
+	if(ans.size() == 0)
+	ans.push_back({-1, -1});
+
+	return ans;
+}
