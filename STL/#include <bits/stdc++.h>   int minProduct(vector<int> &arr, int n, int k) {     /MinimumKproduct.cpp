b10a764d4545dd39b90a54f53f55b86b59db002f
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-08-03 11:43:04 +0530

    Update MinimumKproduct.cpp

--- STL/#include <bits/stdc++.h>   int minProduct(vector<int> &arr, int n, int k) {     /MinimumKproduct.cpp
+++ STL/#include <bits/stdc++.h>   int minProduct(vector<int> &arr, int n, int k) {     /MinimumKproduct.cpp
@@ -20,3 +20,37 @@ int minProduct(vector<int> &arr, int n, int k) {
 
     return prod;
 }
+
+//Alternative Solution
+
+#include <bits/stdc++.h> 
+
+int minProduct(vector<int> &arr, int n, int k) {
+    // Write your code here.
+
+    priority_queue<int, vector<int>> pq;
+    int mod = 1000000000 + 7;
+
+
+    for(int i=0; i<k; i++)
+    pq.push(arr[i]);
+
+    for(int i=k; i<n; i++)
+    {
+        if(arr[i] < pq.top())
+        {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+
+    int prod = 1;
+
+    while(!pq.empty())
+    {
+        prod = (prod *(long long)pq.top())%mod;
+        pq.pop();
+    }
+
+    return prod;
+}
