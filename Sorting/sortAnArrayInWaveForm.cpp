9fb056ad2c3a1be67f2150075d23ef7c4c6ed68e
Sharmista Shastry <33427174+sharmista2shastry@users.noreply.github.com>
2023-09-12 21:26:45 -0400

    Update sortAnArrayInWaveForm.cpp

--- Sorting/sortAnArrayInWaveForm.cpp
+++ Sorting/sortAnArrayInWaveForm.cpp
@@ -1,9 +1,28 @@
+//Time Complexity: O(nlogn)
+
 vector<int> waveFormArray(vector<int> &arr, int n) {
     // Write your code here.
     sort(arr.begin(), arr.end());
 
     for(int i=0; i<n-1; i = i + 2)
     swap(arr[i], arr[i+1]);
+
+    return arr;
+}
+
+// Optimised approach with O(n) time complexity
+
+vector<int> waveFormArray(vector<int> &arr, int n) {
+    // Write your code here.
+    
+    for(int i=0; i<n; i = i+2)
+    {
+        if(i > 0 && arr[i] < arr[i-1])
+        swap(arr[i], arr[i-1]);
+
+        if(i < n-1 && arr[i] < arr[i+1])
+        swap(arr[i], arr[i+1]);
+    }
 
     return arr;
 }
